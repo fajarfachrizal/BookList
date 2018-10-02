99bdbc39cb1ad4520f89bcf517318f5a5fc11bdd
fajarfachrizal <fajar.fachrizal@gmail.com>
2018-10-03 01:23:36 +0200

    change default font for the app
    using font library from https://github.com/InflationX/Calligraphy and use roboto font as a default application font

--- app/src/main/java/de/example/fajar/booklist/MainActivity.java
+++ app/src/main/java/de/example/fajar/booklist/MainActivity.java
@@ -1,15 +1,36 @@
 package de.example.fajar.booklist;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import io.github.inflationx.calligraphy3.CalligraphyConfig;
+import io.github.inflationx.calligraphy3.CalligraphyInterceptor;
+import io.github.inflationx.viewpump.ViewPump;
+import io.github.inflationx.viewpump.ViewPumpContextWrapper;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView mBookListTv;
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(ViewPumpContextWrapper.wrap(newBase));
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ViewPump.init(ViewPump.builder()
+                .addInterceptor(new CalligraphyInterceptor(
+                        new CalligraphyConfig.Builder()
+                                .setDefaultFontPath("fonts/RobotoSlab-Regular.ttf")
+                                .setFontAttrId(R.attr.fontPath)
+                                .build()))
+                .build());
+
         setContentView(R.layout.activity_main);
 
         mBookListTv = (TextView) findViewById(R.id.tv_book_list);
